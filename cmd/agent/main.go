74df9675c65f94e983939d2843570b313b2a4cd3
duleisi <151037155+laoxiechuzheng@users.noreply.github.com>
2026-07-27 17:40:10 +0800

    Update main.go

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -845,7 +845,7 @@ func handleTcpPingTask(task *pb.Task, result *pb.TaskResult) {
 	addr := net.JoinHostPort(ipAddr, port)
 	printf("TCP-Ping Task: Pinging %s", addr)
 	start := time.Now()
-	conn, err := net.DialTimeout("tcp", addr, time.Second*3)
+	conn, err := net.DialTimeout("tcp", addr, 7*time.Second)
 	if err != nil {
 		result.Data = err.Error()
 	} else {
